b0cf8238fc3f4470f4b422a3908863e505ca9aab
mentalblood <neceporenkostepan@gmail.com>
2021-06-09 10:03:15 +0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,18 @@
+/** @jsx h */
+const { h } = preact;
+
+class Root extends preact.Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
+	componentDidMount() {
+	}
+
+	render() {
+		return null;
+	}
+}
+
+preact.render(<Root></Root>);
